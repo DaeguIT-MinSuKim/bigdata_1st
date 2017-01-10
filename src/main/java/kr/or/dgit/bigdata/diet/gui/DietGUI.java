0c75c878124ec76f135d61582beb54c313458a13
naver4ever <hak2gyo@gmail.com>
2017-01-10 15:11:34 +0900

    DietGUI MenuPlanGUI 수정

--- src/main/java/kr/or/dgit/bigdata/diet/gui/DietGUI.java
+++ src/main/java/kr/or/dgit/bigdata/diet/gui/DietGUI.java
@@ -1,30 +1,31 @@
 package kr.or.dgit.bigdata.diet.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
-import javax.swing.JLabel;
-import java.awt.Font;
-import java.awt.Graphics;
-
-import java.awt.Image;
-
-import java.awt.Color;
-import java.awt.Container;
-
+import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class DietGUI extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private Container contentPane;
+	Container contentPane;
+	private JTextField tfName;
+	private JTextField tfGender;
+	private JTextField tfWeight;
+	private JTextField tfAge;
+	private JTextField tfPhone;
+	private JTextField tfAddr;
+	private JTextField tfBudget;
+	private JTextField tfNowNum;
+	private JTextField thWholeNum;
 
 	/**
 	 * Launch the application.
@@ -34,7 +35,6 @@ public class DietGUI extends JFrame {
 			public void run() {
 				try {
 					DietGUI frame = new DietGUI();
-					frame.setTitle("Your Diet Planning");
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -47,115 +47,121 @@ public class DietGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public DietGUI() {
+		setTitle("식단생성");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 930, 600);
+		setBounds(100, 100, 500, 700);
 		contentPane = getContentPane();
-		dietBgPanel dbp = new dietBgPanel();
+		DietBgPanel dbp = new DietBgPanel();
 		contentPane.add(dbp, BorderLayout.CENTER);
 		dbp.setLayout(null);
-
-		// 이미지 아이콘
-		ImageIcon originDay = new ImageIcon("pictogram/day.png");
-		Image originDayImg = originDay.getImage();
-		Image changeDayImg = originDayImg.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
-		ImageIcon changeDay = new ImageIcon(changeDayImg);
-
-		// 메인타이틀
-		JLabel lblTitle = new JLabel("DIET PLANNING FOR 1MONTH");
-		lblTitle.setBounds(10, 30, 419, 45);
-		lblTitle.setFont(new Font("Fugaz One", Font.PLAIN, 30));
-		dbp.add(lblTitle);
-
-		// 버튼
-		JButton btnDay1 = new JButton("");
-		btnDay1.setFocusable(false);
-		btnDay1.setBorder(null);
-		btnDay1.setBackground(Color.WHITE);
-		btnDay1.setBounds(20, 85, 100, 100);
-		btnDay1.setBorderPainted(false);
-		btnDay1.setFocusPainted(false);
-        btnDay1.setContentAreaFilled(false);
-		btnDay1.setIcon(changeDay);
-		dbp.add(btnDay1);
-
-		JButton btnDay2 = new JButton("");
-		btnDay2.setFocusable(false);
-		btnDay2.setBorder(null);
-		btnDay2.setBackground(Color.WHITE);
-		btnDay2.setBounds(150, 85, 100, 100);
-		btnDay2.setFocusPainted(false);
-        btnDay2.setContentAreaFilled(false);
-		btnDay2.setIcon(changeDay);
-		btnDay2.setIcon(changeDay);
-		dbp.add(btnDay2);
-
-		JButton btnDay3 = new JButton("");
-		btnDay3.setFocusable(false);
-		btnDay3.setBorder(null);
-		btnDay3.setBackground(Color.WHITE);
-		btnDay3.setBounds(280, 85, 100, 100);
-		btnDay3.setFocusPainted(false);
-        btnDay3.setContentAreaFilled(false);
-		btnDay3.setIcon(changeDay);
-		btnDay3.setIcon(changeDay);
-		dbp.add(btnDay3);
-
-		JButton btnDay4 = new JButton("");
-		btnDay4.setFocusable(false);
-		btnDay4.setBorder(null);
-		btnDay4.setBackground(Color.WHITE);
-		btnDay4.setBounds(410, 85, 100,100);
-		btnDay4.setFocusPainted(false);
-        btnDay4.setContentAreaFilled(false);
-		btnDay4.setIcon(changeDay);
-		btnDay4.setIcon(changeDay);
-		dbp.add(btnDay4);
-
-		JButton btnDay5 = new JButton("");
-		btnDay5.setFocusable(false);
-		btnDay5.setBorder(null);
-		btnDay5.setBackground(Color.WHITE);
-		btnDay5.setBounds(540, 85, 100, 100);
-		btnDay5.setFocusPainted(false);
-        btnDay5.setContentAreaFilled(false);
-		btnDay5.setIcon(changeDay);
-		btnDay5.setIcon(changeDay);
-		dbp.add(btnDay5);
-
-		JButton btnDay6 = new JButton("");
-		btnDay6.setFocusable(false);
-		btnDay6.setBorder(null);
-		btnDay6.setBackground(Color.WHITE);
-		btnDay6.setBounds(670, 85, 100, 100);
-		btnDay6.setFocusPainted(false);
-        btnDay6.setContentAreaFilled(false);
-		btnDay6.setIcon(changeDay);
-		btnDay6.setIcon(changeDay);
-		dbp.add(btnDay6);
-
-		JButton btnDay7 = new JButton("");
-		btnDay7.setFocusable(false);
-		btnDay7.setBorder(null);
-		btnDay7.setBackground(Color.WHITE);
-		btnDay7.setBounds(800, 85, 100, 100);
-		btnDay7.setFocusPainted(false);
-        btnDay7.setContentAreaFilled(false);
-		btnDay7.setIcon(changeDay);
-		btnDay7.setIcon(changeDay);
-		dbp.add(btnDay7);
-
+		
+		tfName = new JTextField();
+		tfName.setText("ex)임정학");
+		tfName.setBounds(37, 193, 70, 21);
+		dbp.add(tfName);
+		tfName.setColumns(10);
+		
+		tfGender = new JTextField();
+		tfGender.setText("남");
+		tfGender.setBounds(113, 193, 33, 21);
+		dbp.add(tfGender);
+		tfGender.setColumns(10);
+		
+		tfWeight = new JTextField();
+		tfWeight.setText("60");
+		tfWeight.setBounds(37, 224, 33, 21);
+		dbp.add(tfWeight);
+		tfWeight.setColumns(10);
+		
+		tfAge = new JTextField();
+		tfAge.setText("28");
+		tfAge.setColumns(10);
+		tfAge.setBounds(94, 224, 33, 21);
+		dbp.add(tfAge);
+		
+		tfPhone = new JTextField();
+		tfPhone.setText("01012341234");
+		tfPhone.setBounds(78, 306, 187, 21);
+		dbp.add(tfPhone);
+		tfPhone.setColumns(10);
+		
+		tfAddr = new JTextField();
+		tfAddr.setText("대구광역시");
+		tfAddr.setColumns(10);
+		tfAddr.setBounds(78, 356, 187, 21);
+		dbp.add(tfAddr);
+		
+		tfBudget = new JTextField();
+		tfBudget.setText("1,000,000");
+		tfBudget.setColumns(10);
+		tfBudget.setBounds(78, 410, 187, 21);
+		dbp.add(tfBudget);
+		
+		tfNowNum = new JTextField();
+		tfNowNum.setText("1");
+		tfNowNum.setBounds(331, 30, 42, 40);
+		dbp.add(tfNowNum);
+		tfNowNum.setColumns(10);
+		
+		thWholeNum = new JTextField();
+		thWholeNum.setText("20");
+		thWholeNum.setColumns(10);
+		thWholeNum.setBounds(331, 87, 42, 40);
+		dbp.add(thWholeNum);
+		
+		JButton btnPrev = new JButton("Prev");
+		btnPrev.setBounds(96, 507, 97, 23);
+		dbp.add(btnPrev);
+		
+		JButton btnNext = new JButton("Next");
+		btnNext.setBounds(289, 507, 97, 23);
+		dbp.add(btnNext);
+		
+		JButton btnPlanA = new JButton("plan A");
+		btnPlanA.setBounds(37, 579, 64, 61);
+		dbp.add(btnPlanA);
+		
+		JButton btnPlanB = new JButton("plan B");
+		btnPlanB.setBounds(113, 579, 64, 61);
+		dbp.add(btnPlanB);
+		
+		JButton btnPlanC = new JButton("plan C");
+		btnPlanC.setBounds(201, 579, 64, 61);
+		dbp.add(btnPlanC);
+		
+		JLabel lblWeight = new JLabel("KG");
+		lblWeight.setBounds(70, 227, 57, 15);
+		dbp.add(lblWeight);
+		
+		JLabel lblAge = new JLabel("세");
+		lblAge.setBounds(127, 225, 19, 18);
+		dbp.add(lblAge);
+		
+		JLabel lblPhone = new JLabel("Phone");
+		lblPhone.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblPhone.setBounds(13, 309, 57, 15);
+		dbp.add(lblPhone);
+		
+		JLabel lblAddr = new JLabel("Address");
+		lblAddr.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblAddr.setBounds(13, 359, 57, 15);
+		dbp.add(lblAddr);
+		
+		JLabel lblBudget = new JLabel("Budget");
+		lblBudget.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblBudget.setBounds(13, 413, 57, 15);
+		dbp.add(lblBudget);
 	}
 }
 
-class dietBgPanel extends JPanel{
-
-	private static final long serialVersionUID = 1L;
-	ImageIcon DietBgIcon = new ImageIcon("images/healthy_food (6).jpg");
-	Image DietBgImg = DietBgIcon.getImage();
+class DietBgPanel extends JPanel{
+	ImageIcon dbgIcon = new ImageIcon("images/signup_image_origin.jpg");
+	Image dbgImg = dbgIcon.getImage();
+	
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		
-		g.drawImage(DietBgImg, 0, 0, getWidth(), getHeight(), this);
+		g.drawImage(dbgImg, 0, 0, getWidth(), getHeight(), this);
 	}
 }
