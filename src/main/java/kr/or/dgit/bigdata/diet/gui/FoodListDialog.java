61699f5e0aae71b7e30bd95c91203ce410f86029
bomikang <bomiisworldstar@gmail.com>
2017-01-18 11:57:18 +0900

    FoodList 수정중

--- src/main/java/kr/or/dgit/bigdata/diet/gui/FoodListDialog.java
+++ src/main/java/kr/or/dgit/bigdata/diet/gui/FoodListDialog.java
@@ -16,9 +16,9 @@ public class FoodListDialog extends JDialog {
 	public FoodListDialog() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
 		
 		//현재 다이얼로그를 띄웠을 때 다른 프레임은 움직이지 못하도록 처리
 		setModalityType(ModalityType.APPLICATION_MODAL);
