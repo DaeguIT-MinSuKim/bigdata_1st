0df5f120a58e784ac2c2a26e58e0e92ae367897d
bomikang <bomiisworldstar@gmail.com>
2017-01-18 12:01:32 +0900

    식산생성 => 식단추가식사시간 추가

--- src/main/java/kr/or/dgit/bigdata/diet/gui/MainStartGUI.java
+++ src/main/java/kr/or/dgit/bigdata/diet/gui/MainStartGUI.java
@@ -210,7 +210,7 @@ public class MainStartGUI extends JFrame implements ActionListener {
 		lblSearch.setBounds(410, 310, 120, 20);
 		panel.add(lblSearch);
 		
-		JLabel lblDiet = new JLabel("식단생성");
+		JLabel lblDiet = new JLabel("식단추가");
 		lblDiet.setHorizontalAlignment(SwingConstants.CENTER);
 		lblDiet.setFont(new Font("10X10", Font.PLAIN, 20));
 		lblDiet.setBounds(535, 310, 120, 20);
@@ -336,13 +336,33 @@ public class MainStartGUI extends JFrame implements ActionListener {
 			ArrayList<Menu> templistoneday = list.get(i).menuList;
 
 			for(int j=0;j<templistoneday.size();j++){
+				int quo = templistoneday.size() / 3; // (하루치 메뉴 개수 / 아침,점심,저녁)
+				int rem = templistoneday.size() % 3; // (하루치 메뉴 개수 / 아침,점심,저녁)의
+														// 나머지 개수
+				String str = ""; // 아침 점심 저녁을 저장할 변수
+
+				/*
+				 * 6만약 메뉴 개수가 7개일 때 몫 = 2, 나머지 =1 
+				 * 아침 2개, 점심 3개, 저녁 2개 
+				 * 아침 = 1, 2
+				 * 점심 = 2, 3, 4 
+				 * 저녁 = 5, 6
+				 */
+				if (j < quo) {
+					str = "아침";
+				} else if (j < (quo + quo + rem)) {
+					str = "점심";
+				} else if (j < (quo + quo + quo + rem)) {
+					str = "저녁";
+				}
+
 				ttt++;
 				
 				   //"번호","일자","식사","항목", "메뉴", "칼로리(cal)", "지방", "탄수화물", "단백질", "비용"
 				rowDatas[ttt] = new String[]{
 						(ttt+1)+"",
 						(i+1)+"",
-						"",
+						str,
 						templistoneday.get(j).getGrp()+ "",
 						templistoneday.get(j).getItem()+ "",
 						templistoneday.get(j).getCal()+ "",
